ddf820ea1b7b7605deb597d652ef13c2a5b96a2b
MrGoc <mrgoc@live.com>
2023-04-21 13:06:01 +0200

    feat: Implement route /api/translate

--- routes/api.js
+++ routes/api.js
@@ -1,13 +1,39 @@
-'use strict';
+"use strict";
 
-const Translator = require('../components/translator.js');
+const { json } = require("body-parser");
+const Translator = require("../components/translator.js");
+
+const amToBr = "american-to-british";
+const brToAm = "british-to-american";
 
 module.exports = function (app) {
-  
   const translator = new Translator();
 
-  app.route('/api/translate')
-    .post((req, res) => {
-      
-    });
+  app.route("/api/translate").post((req, res) => {
+    let text = req.body.text;
+    let locale = req.body.locale;
+    if (!text || !locale) {
+      res.json({ error: "Required field(s) missing" });
+      return;
+    }
+    if (text === "") {
+      res.json({ error: "No text to translate" });
+      return;
+    }
+    if (locale !== amToBr || locale != brToAm) {
+      res.json({ error: "Invalid value for locale field" });
+      return;
+    }
+    let transText = translator.translate(text);
+    if (transText === text)
+      res.json({
+        text: text,
+        translation: "Everything looks good to me!",
+      });
+    else
+      res.json({
+        text: text,
+        translation: transText,
+      });
+  });
 };
